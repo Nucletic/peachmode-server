a4a6eeeff18ec7152e87583e9f871275faa70faa
nucletic <kuntalpriyanshu608@gmail.com>
2024-07-20 15:21:36 +0530

    asdf

--- server.js
+++ server.js
@@ -20,11 +20,18 @@ const PORT = process.env.PORT || 5000;
 const corsOption = {
   credentials: true,
   origin: ["http://localhost:3000", "https://peach-mode.netlify.app/"],
-  // origin: "https://peach-mode.netlify.app/",
 }
 
 
-app.use(cors(corsOption));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://peach-mode.netlify.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+// app.use(cors(corsOption));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
