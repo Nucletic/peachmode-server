3ae6d2136d584fcb6de1dda7df5ee6b94a933db5
nucletic <kuntalpriyanshu608@gmail.com>
2024-07-20 15:09:23 +0530

    asdf

--- server.js
+++ server.js
@@ -24,15 +24,7 @@ const corsOption = {
 }
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
-
-app.use(cors(corsOption));
-app.options('*', cors(corsOption));
+app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
